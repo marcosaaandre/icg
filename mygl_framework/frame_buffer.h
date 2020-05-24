d8cd560987ced9e9e1de1ae2aee4bdb0720f7501
capagot <christian.azambuja@gmail.com>
2020-05-24 19:13:13 -0300

    mygl_framework added.

---
+++ mygl_framework/frame_buffer.h
@@ -0,0 +1,7 @@
+#ifndef FRAME_BUFFER_H
+#define FRAME_BUFFER_H
+
+// Ponteiro que aponta para o prmeiro byte do color buffer.
+unsigned char* fb_ptr;
+
+#endif  // FRAME_BUFFER_H
---
+++ mygl_framework/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <GL/glut.h>
+#include <stdio.h>
+
+#include "core.h"
+#include "mygl.h"
+
+GLuint tex;
+
+#endif  // MAIN_H
